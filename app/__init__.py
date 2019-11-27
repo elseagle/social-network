c471fe7f5450d2f1610487d294ced2176a4fc824
Sogo Ogundowole <dowolebolu@gmail.com>
2019-11-27 17:48:08 +0100

    added register route

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,18 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config
 from flask_login import LoginManager
 from flask_pagedown import PageDown
-
-
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
-
 pagedown = PageDown()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -31,13 +29,17 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
-    from .main import main as main_blueprint
-    from .auth import auth as auth_blueprint
-    from .api import api as api_blueprint
+    if app.config['SSL_REDIRECT']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
 
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
-
