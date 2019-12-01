42cdd0991edfa6d030dc8535057293901f83d2e6
Sogo Ogundowole <dowolebolu@gmail.com>
2019-12-01 19:45:13 +0100

    added docker-compose and used loop or entry bash script

--- boot.sh
+++ boot.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 source venv/bin/activate
-flask deploy
+
+while true; do
+    flask deploy
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+
 exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - flasky:app
-
-
--e DATABASE_URL=mysql+pymysql://flasky:password@dbserver/flasky \
--e MAIL_DEFAULT_FROM=dowolebolu@gmail.com -e \ flasky:latest
