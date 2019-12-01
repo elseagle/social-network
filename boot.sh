90532ba8b314b7c289f72ee10d5a428f7ff7f877
Sogo Ogundowole <dowolebolu@gmail.com>
2019-12-01 18:42:11 +0100

    added mysql db image

--- boot.sh
+++ boot.sh
@@ -2,3 +2,7 @@
 source venv/bin/activate
 flask deploy
 exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - flasky:app
+
+
+-e DATABASE_URL=mysql+pymysql://flasky:password@dbserver/flasky \
+-e MAIL_DEFAULT_FROM=dowolebolu@gmail.com -e \ flasky:latest
